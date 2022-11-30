95d5a10a084bccdf88629dc7155c26cfc4204734
Alexander Maslov <steam3d@gmail.com>
2022-11-30 16:46:25 +0300

    Updated ArmDot. Added some code for testing.

--- FullTrust/Program.cs
+++ FullTrust/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+[assembly: ArmDot.Client.HideStrings()]
+[assembly: ArmDot.Client.ObfuscateNamesAttribute()]
+[assembly: ArmDot.Client.ObfuscateNamespaces()]
+[assembly: ArmDot.Client.ObfuscateControlFlow()]
+
+//[assembly: ArmDot.Client.VirtualizeCode()]
 namespace FullTrust
 {
     class Program
@@ -12,8 +14,33 @@ namespace FullTrust
         {
             Console.Title = "Hello World";
             Console.WriteLine("This process runs at the full privileges of the user and has access to the entire public desktop API surface");
+            var i = UselessMethod();
+            var useless = new UselessClass(i, "Main");
+            Console.WriteLine($"{useless.Count} =  {useless.Tag}");
             Console.WriteLine("\r\nPress any key to exit ...");
             Console.ReadLine();
+        }
+
+        private static int UselessMethod()
+        {
+            return 5;
+        }
+
+        internal class UselessClass
+        {
+            private int count = 0;
+
+            private string tag = "UselessClass";
+
+            public int Count { get { return count; } }
+
+            public string Tag { get { return tag; } }
+            
+            public UselessClass(int count, string tag)
+            {
+                this.count = count;
+                this.tag = tag;            
+            }
         }
     }
 }
