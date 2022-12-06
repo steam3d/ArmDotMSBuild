016e76a364cd4396402b031ce1afbb03483d0101
Alexander Maslov <steam3d@gmail.com>
2022-12-06 18:45:58 +0300

    Sample ready

---
+++ FullTrust/IKsControl.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace Interop
+{
+    [ComImport, SuppressUnmanagedCodeSecurity,
+        Guid("28F54685-06FD-11D2-B27A-00A0C9223196"),
+        InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IKsControl
+    {
+        [PreserveSig]
+        int KsProperty(
+          [In] ref KSMETHOD Property,
+          [In] Int32 PropertyLength,
+          [In, Out] IntPtr PropertyData,
+          [In] Int32 DataLength,
+          [In, Out] ref Int32 BytesReturned
+          );
+
+        [PreserveSig]
+        int KsMethod(
+          [In] ref KSMETHOD Method,
+          [In] Int32 MethodLength,
+          [In, Out] IntPtr MethodData,
+          [In] Int32 DataLength,
+          [In, Out] ref Int32 BytesReturned
+          );
+
+        [PreserveSig]
+        int KsEvent(
+          [In, Optional] ref KSMETHOD Event,
+          [In] Int32 EventLength,
+          [In, Out] IntPtr EventData,
+          [In] Int32 DataLength,
+          [In, Out] ref Int32 BytesReturned
+          );
+    }
+
+    internal struct KSMETHOD
+    {
+        public Guid Set;
+        public Int32 Id;
+        public Int32 Flags;
+
+        public KSMETHOD(Guid set, Int32 id, Int32 flags)
+        {
+            Set = set;
+            Id = (Int32)id;
+            Flags = (Int32)flags;
+        }
+    }
+}
--- FullTrust/Program.cs
+++ FullTrust/Program.cs
@@ -14,33 +14,8 @@ namespace FullTrust
         {
             Console.Title = "Hello World";
             Console.WriteLine("This process runs at the full privileges of the user and has access to the entire public desktop API surface");
-            var i = UselessMethod();
-            var useless = new UselessClass(i, "Main");
-            Console.WriteLine($"{useless.Count} =  {useless.Tag}");
             Console.WriteLine("\r\nPress any key to exit ...");
             Console.ReadLine();
-        }
-
-        private static int UselessMethod()
-        {
-            return 5;
-        }
-
-        internal class UselessClass
-        {
-            private int count = 0;
-
-            private string tag = "UselessClass";
-
-            public int Count { get { return count; } }
-
-            public string Tag { get { return tag; } }
-            
-            public UselessClass(int count, string tag)
-            {
-                this.count = count;
-                this.tag = tag;            
-            }
         }
     }
 }
